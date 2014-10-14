bb834cfa8a4c97e031c29f0ee12e645e485b5048
Nikita Kniazev <nok.raven@gmail.com>
2014-10-14 15:12:37 +0400

    Redis dispatcher timeouts configuration

--- plugins/dispatcher_redis/dispatcher_redis.cpp
+++ plugins/dispatcher_redis/dispatcher_redis.cpp
@@ -32,11 +32,14 @@ dispatcher_redis::load_settings(const boost::property_tree::ptree& settings)
   host_ = settings.get<std::string>("host", "localhost");
   port_ = settings.get<std::string>("port", "6379");
   pretty_ = settings.get<bool>("pretty", false);
+  std::size_t timeout_connect = settings.get<std::size_t>("timeout.connect", 10);
+  std::size_t timeout_ping = settings.get<std::size_t>("timeout.ping", 30);
 
   BOOST_LOG_SEV(log_, logging::trace)
     << name() << " is configured to " << host_ << ":" << port_;
 
   rcon_ = boost::shared_ptr<redis_connection>(new redis_connection(api_->io_service()));
+  rcon_->set_timeouts(timeout_connect, timeout_ping);
   rcon_->connect(host_, port_);
 }
 
